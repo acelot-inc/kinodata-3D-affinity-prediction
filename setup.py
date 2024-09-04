20b5d3810bd2a0d4761a462622e7bf6c666fa472
Cody Aldaz <cody@acelot.com>
2024-09-04 23:10:37 +0000

    🐛 FIX: maybe another bug in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 
-
-def get_version() -> str:
-    return "1.0.0"
-
-
-print(find_packages())
-
 setup(
     name="kinodata",
-    version=get_version(),
-    packages=find_packages(include=["kinodata", "kinodata.transform", "kinodata.model", "kinodata.training", "kinodata.data", "kinodata.data.featurization", "kinodata.data.io", "kinodata.data.utils"]),
-
+    version="1.0.0",
+    packages=find_packages(include=[
+        "kinodata",
+        "kinodata.transform",
+        "kinodata.model",
+        "kinodata.training",
+        "kinodata.data",
+        "kinodata.data.featurization",
+        "kinodata.data.io",
+        "kinodata.data.utils"
+    ]),
 )
